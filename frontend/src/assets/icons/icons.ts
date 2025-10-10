4324b4f94026829394e61312f9bff4499267c8f8
Foronda Yna Gabrielle (External) <yna.foronda@betterask.erni>
2025-10-10 17:31:04 +0800

    Polished home page looks with additional assets

---
+++ frontend/src/assets/icons/icons.ts
@@ -0,0 +1,16 @@
+// src/assets/icons.ts
+import puzzle from "./puzzle.svg";
+import sudoku from "./sudoku.svg";
+import wordle from "./wordle.svg";
+import hangman from "./hangman.svg";
+import crossword from "./crossword.svg";
+import connections from "./connections.svg";
+
+export {
+    puzzle,
+    sudoku,
+    wordle,
+    hangman,
+    crossword,
+    connections
+};
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -6,7 +6,7 @@ export default function Navbar() {
     const [isOpen, setIsOpen] = useState(false);
 
     return (
-        <header className="fixed top-0 left-0 w-full bg-white shadow-md z-50">
+        <header className="fixed top-0 left-0 w-full bg-slate-50 shadow-md z-50">
             <div
                 className="mx-auto w-full max-w-[80rem] border transition-colors rounded-lg my-[10px] border-transparent bg-transparent"
                 role="menu"
@@ -16,11 +16,13 @@ export default function Navbar() {
                         <a className="absolute left-4 flex items-center h-full" href="/">
                             <img className="h-14 w-auto" src={logo} alt="Namespace Logo" />
                         </a>
+
                         <nav className="absolute left-0 right-0 flex justify-center items-center space-x-6">
-                            <a href="/#" className="bg-primary font-medium text-md text-white px-4 py-2 rounded-md shadow-[0_5px_0_0_#033778] active:shadow-[0_4px_0_0_rgba(0,0,0,0.15)] translate-y-[-2px] active:translate-y-0" type="button">
+                            <a href="/#" className="bg-primary font-medium text-md text-slate-50 px-4 py-2 rounded-md shadow-[0_5px_0_0_#033778] active:shadow-[0_4px_0_0_rgba(0,0,0,0.15)] translate-y-[-2px] active:translate-y-0" type="button">
                                 Home
                             </a>
                             <a className="font-semibold text-primary" href="/leaderboards">Leaderboards</a>
+                            <a className="font-semibold text-primary">┃</a>
                             <a className="font-semibold text-primary" href="/sudoku">Sudoku</a>
                             <a className="font-semibold text-primary" href="/wordle">Wordle</a>
                             <a className="font-semibold text-primary" href="/hangman">Hangman</a>
@@ -28,6 +30,7 @@ export default function Navbar() {
                             <a className="font-semibold text-primary" href="/connections">Connections</a>
                         </nav>
                         
+                        <img aria-expanded="false" aria-haspopup="menu" id="profile-menu" src="https://raw.githubusercontent.com/creativetimofficial/public-assets/master/ct-assets/team-4.jpg" alt="profile-picture" className="inline-block object-cover object-center size-12 rounded-full outline-none group border border-slate-800 p-0.5 lg:ml-auto"></img>
                     </div>
                 </div>
             </div>
