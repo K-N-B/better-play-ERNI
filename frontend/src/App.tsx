d84ebfe4f844fb09ef976e7a80fd9139a8ca29b5
Yna Foronda <Yna's PC>
2025-10-11 19:06:35 +0800

    Moved navbar to App.tsx so it will appear for all pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,41 +1,25 @@
-import Home from "./home";
+
 import { Routes, Route } from "react-router-dom";
+
+
+import Navbar from "./components/Navbar"; 
+import Home from "./home";
 import SudokuGame from './games/sudoku';
 import HangmanGame from './games/hangman';
 
 
-
 function App() {
-  // const [count, setCount] = useState(0)
 
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/sudoku" element={<SudokuGame />} />
-      <Route path="/hangman" element={<HangmanGame />} />
-    </Routes>
-    // <>
-    //   <div>
-    //     <a href="https://vite.dev" target="_blank">
-    //       <img src={viteLogo} className="logo" alt="Vite logo" />
-    //     </a>
-    //     <a href="https://react.dev" target="_blank">
-    //       <img src={reactLogo} className="logo react" alt="React logo" />
-    //     </a>
-    //   </div>
-    //   <h1>Vite + React</h1>
-    //   <div className="card">
-    //     <button onClick={() => setCount((count) => count + 1)}>
-    //       count is {count}
-    //     </button>
-    //     <p>
-    //       Edit <code>src/App.tsx</code> and save to test HMR
-    //     </p>
-    //   </div>
-    //   <p className="read-the-docs">
-    //     Click on the Vite and React logos to learn more
-    //   </p>
-    // </>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sudoku" element={<SudokuGame />} />
+        <Route path="/hangman" element={<HangmanGame />} />
+      </Routes>
+    </>
+    
   )
 }
 
