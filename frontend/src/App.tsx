bb628f2b4ab52328620f48476fa76d5cad2cd743
Yna Foronda <Yna's PC>
2025-10-11 22:42:01 +0800

    minor changes to base page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,12 +14,18 @@ function App() {
   return (
     <>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/leaderboards" element={<Leaderboards />} />
-        <Route path="/sudoku" element={<SudokuGame />} />
-        <Route path="/hangman" element={<HangmanGame />} />
-      </Routes>
+      <div className="min-h-screen h-full w-full bg-[#F1ECE6] absolute inset-0 bg-[linear-gradient(to_right,#D2B694_2px,transparent_1px),linear-gradient(to_bottom,#D2B694_2px,transparent_1px)] bg-[size:24px_24px]">
+      <main className="w-full h-dvh mx-auto pt-20 px-6 ">
+        <div className="flex h-full w-full p-8">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/leaderboards" element={<Leaderboards />} />
+            <Route path="/sudoku" element={<SudokuGame />} />
+            <Route path="/hangman" element={<HangmanGame />} />
+          </Routes>
+        </div> 
+      </main>
+      </div>
     </>
     
   )
