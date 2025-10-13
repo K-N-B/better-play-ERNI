4a5766b358fee540bb15b6f1c44c7c0cfbdd02ec
gavin-cii <forondayna1214@gmail.com>
2025-10-13 15:48:33 +0800

    Removed extra code

--- frontend/src/leaderboards.tsx
+++ frontend/src/leaderboards.tsx
@@ -1,18 +1,6 @@
-import { Dropdown, DropdownItem } from "flowbite-react";
+
 import "flowbite"
 import { useState, useEffect } from "react";
-import { puzzle, sudoku, wordle, hangman, crossword, connections } from "./assets/icons/icons";
-
-const games = [
-  { title: "Games", subtitle: "Choose your poison for today or experience all of them!", color: "bg-slate-50 text-black shadow-slate-50", icon: puzzle },
-  { title: "Sudoku", subtitle: "Sharpen your logic — fill the grid without repeating numbers.", color: "bg-sky-400 text-slate-50 shadow-sky-800", icon: sudoku },
-  { title: "Wordle", subtitle: "Guess the hidden word in six tries or less.", color: "bg-emerald-500 text-slate-50 shadow-emerald-900", icon: wordle },
-  { title: "Hangman", subtitle: "Save the stickman — reveal the word before time runs out.", color: "bg-pink-400 text-slate-50 shadow-pink-800", icon: hangman },
-  { title: "Crossword", subtitle: "Test your vocabulary and wit by solving the daily word clues.", color: "bg-yellow-400 text-slate-50 shadow-yellow-800", icon: crossword },
-  { title: "Connections", subtitle: "Group words into four hidden categories and find the link.", color: "bg-purple-500 text-slate-50 shadow-purple-900", icon: connections },
-];
-
-
 
 export default function Leaderboards() {
   
