9af0da0b2f00152fd16f91856129495eb1486f0d
Perez Yughie (External) <yughie.perez@betterask.erni>
2025-10-14 18:07:29 +0800

    Responsiveness update and bug fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,18 @@
-
 import { Routes, Route } from "react-router-dom";
 
-
-import Navbar from "./components/Navbar"; 
+import Navbar from "./components/Navbar";
 import Home from "./home";
 import Leaderboards from "./leaderboards";
-import SudokuGame from './games/sudoku';
-import HangmanGame from './games/hangman';
-
+import SudokuGame from "./games/sudoku";
+import HangmanGame from "./games/hangman";
 
 function App() {
-
   return (
     <>
-      
       <div className="flex flex-col h-dvh w-dvw bg-[#F1ECE6]  bg-[linear-gradient(to_right,#D2B694_2px,transparent_1px),linear-gradient(to_bottom,#D2B694_2px,transparent_1px)] bg-[size:24px_24px]">
         <Navbar />
-        <main className="h-full w-full overflow-hidden">
-          <div className="h-full w-full p-10">
+        <main className="md:h-full md:w-full md:overflow-hidden">
+          <div className="h-full w-full p-5 md:p-10">
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/leaderboards" element={<Leaderboards />} />
@@ -28,8 +23,7 @@ function App() {
         </main>
       </div>
     </>
-    
-  )
+  );
 }
 
 export default App;
