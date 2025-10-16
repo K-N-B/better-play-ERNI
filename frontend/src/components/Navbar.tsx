eae68356a0fb68e5233fef957ef949d5e80c3d50
gavin-cii <forondayna1214@gmail.com>
2025-10-16 13:18:00 +0800

    Added components for game button and primary button

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,6 +1,7 @@
 // src/components/Navbar.tsx
 import {Link, useLocation} from "react-router-dom";
 import logo from "../assets/logo.png"; // adjust if your logo path differs
+import PrimaryButton from "./PrimaryButton";
 
 export default function Navbar() {
     
@@ -37,28 +38,12 @@ export default function Navbar() {
                                 // Only render Link if path is defined
                                 if (item.path) {
                                     return (
-                                        <Link
-                                            key={item.path}
-                                            to={item.path}
-                                            className={`font-semibold text-primary px-4 py-2 rounded-md 
-                                                ${location.pathname === item.path ? item.color : ''}`}
-                                        >
-                                            {item.name}
-                                        </Link>
+                                        <PrimaryButton text={item.name} color={item.color} path={item.path} />
                                     );
                                 }
                                 return null;
                             })}
-                            {/* <a href="/#" className="bg-primary font-medium text-md text-slate-50 px-4 py-2 rounded-md shadow-[0_5px_0_0_#033778] active:shadow-[0_4px_0_0_rgba(0,0,0,0.15)] translate-y-[-2px] active:translate-y-0" type="button">
-                                Home
-                            </a>
-                            <a className="font-semibold text-primary" href="/leaderboards">Leaderboards</a>
-                            <a className="font-semibold text-primary">┃</a>
-                            <a className="font-semibold text-primary" href="/sudoku">Sudoku</a>
-                            <a className="font-semibold text-primary" href="/wordle">Wordle</a>
-                            <a className="font-semibold text-primary" href="/hangman">Hangman</a>
-                            <a className="font-semibold text-primary" href="/crossword">Crossword</a>
-                            <a className="font-semibold text-primary" href="/connections">Connections</a> */}
+                        
                         </nav>
                         
                         <img aria-expanded="false" aria-haspopup="menu" id="profile-menu" src="https://raw.githubusercontent.com/creativetimofficial/public-assets/master/ct-assets/team-4.jpg" alt="profile-picture" className="inline-block object-cover object-center size-12 rounded-full outline-none group border border-slate-800 p-0.5 lg:ml-auto"></img>
