2dc2b1381fd7cc2f9ce07026024d2a23e7425917
dayniel-caadiang <dayniel.caadiang@betterask.erni>
2025-10-16 16:57:02 +0800

    Merged with main

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,8 +3,6 @@ import Login from "./components/Login";
 import AuthCallback from "./components/AuthCallback";
 import ProtectedRoute from "./components/ProtectedRoute";
 import Layout from "./components/Layout"; // Layout includes Navbar
-
-import Navbar from "./components/Navbar";
 import Home from "./home";
 import Leaderboards from "./leaderboards";
 import SudokuGame from "./games/sudoku";
@@ -13,59 +11,82 @@ import HangmanGame from "./games/hangman";
 function App() {
   return (
     <>
-      <div className="flex flex-col h-dvh w-dvw bg-[#F1ECE6]  bg-[linear-gradient(to_right,#D2B694_2px,transparent_1px),linear-gradient(to_bottom,#D2B694_2px,transparent_1px)] bg-[size:24px_24px]">
-        <Navbar />
-        <main className="md:h-full md:w-full md:overflow-hidden">
-          <div className="h-full w-full p-5 md:p-10">
-            <Routes>
-              <Route path="/login" element={<Login />} />
-              <Route path="/auth/callback" element={<AuthCallback />} />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/auth/callback" element={<AuthCallback />} />
 
-              {/* Protected routes wrapped with Layout (Navbar + content) */}
-              <Route
-                path="/"
-                element={
-                  <ProtectedRoute>
-                    <Layout>
-                      <Home />
-                    </Layout>
-                  </ProtectedRoute>
-                }
-              />
-              <Route
-                path="/leaderboards"
-                element={
-                  <ProtectedRoute>
-                    <Layout>
-                      <Leaderboards />
-                    </Layout>
-                  </ProtectedRoute>
-                }
-              />
-              <Route
-                path="/sudoku"
-                element={
-                  <ProtectedRoute>
-                    <Layout>
-                      <SudokuGame />
-                    </Layout>
-                  </ProtectedRoute>
-                }
-              />
-              <Route
-                path="/hangman"
-                element={
-                  <ProtectedRoute>
-                    <Layout>
-                      <HangmanGame />
-                    </Layout>
-                  </ProtectedRoute>
-                }
-              />
-            </Routes>
-          </div>
-        </main>
-      </div>
+        {/* Protected routes wrapped with Layout (Navbar + content) */}
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Layout>
+                <Home />
+              </Layout>
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/leaderboards"
+          element={
+            <ProtectedRoute>
+              <Layout>
+                <Leaderboards />
+              </Layout>
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/sudoku"
+          element={
+            <ProtectedRoute>
+              <Layout>
+                <SudokuGame />
+              </Layout>
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/wordle"
+          element={
+            <ProtectedRoute>
+              <Layout>
+                <div>Wordle Game Coming Soon!</div>
+              </Layout>
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/hangman"
+          element={
+            <ProtectedRoute>
+              <Layout>
+                <HangmanGame />
+              </Layout>
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/crossword"
+          element={
+            <ProtectedRoute>
+              <Layout>
+                <div>Crossword Game Coming Soon!</div>
+              </Layout>
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/connections"
+          element={
+            <ProtectedRoute>
+              <Layout>
+                <div>Connections Game Coming Soon!</div>
+              </Layout>
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
     </>
   );
     
--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -8,10 +8,17 @@ interface LayoutProps {
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <>
-      <Navbar />
-      <main>{children}</main>
+      <div className="flex flex-col h-dvh w-dvw bg-[#F1ECE6]  bg-[linear-gradient(to_right,#D2B694_2px,transparent_1px),linear-gradient(to_bottom,#D2B694_2px,transparent_1px)] bg-[size:24px_24px]">
+        <Navbar />
+        <main className="md:h-full md:w-full md:overflow-hidden">
+          <div className="h-full w-full p-5 md:p-10">
+            {children}
+          </div>
+        </main>
+      </div>
     </>
   );
-};
+}
+  
 
 export default Layout;
