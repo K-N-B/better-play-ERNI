20f457e267466c0f1619b2a3a3ecbc8bbc4a9218
gavin-cii <forondayna1214@gmail.com>
2025-10-20 07:43:24 +0800

    Moved obsolete files

--- frontend/src/components/layout/layout.tsx
+++ frontend/src/components/layout/layout.tsx
@@ -9,7 +9,7 @@ export const Layout = () => {
   const { user } = useAuth();
 
   return (
-    <div className="min-h-screen bg-gray-50">
+    <div className="min-h-screen bg-[#F1ECE6]  bg-[linear-gradient(to_right,#D2B694_2px,transparent_1px),linear-gradient(to_bottom,#D2B694_2px,transparent_1px)] bg-[size:24px_24px]">
       <Navbar />
       <main>
         {/* Render the current page (e.g., HomePage) */}
