df701e5cf50e1591d0ceb7a14bca01aed33b04f3
gavin-cii <forondayna1214@gmail.com>
2025-10-24 17:12:38 +0800

    Deleted obsolete folders/files

--- puzzle_backend/gameplay/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import PuzzleAttempt
-
-admin.site.register(PuzzleAttempt)
--- puzzle_backend/games/views.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import viewsets
-from .models import DailyPuzzle
-from .serializers import DailyPuzzleSerializer
-from rest_framework.permissions import AllowAny  # change later
-
-class DailyPuzzleViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = DailyPuzzle.objects.select_related('wordle','sudoku','ernigram').all()
-    serializer_class = DailyPuzzleSerializer
-    permission_classes = [AllowAny]  # for front-end testing switch to IsAuthenticated later
