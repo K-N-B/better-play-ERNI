10b4d22d06d22b4c9cc4731d2c11b933c06bb378
Yughie <yughiep@gmail.com>
2025-11-02 12:26:29 +0800

    Configuration deployment setting

--- puzzle_backend/config/settings.py
+++ puzzle_backend/config/settings.py
@@ -113,7 +113,7 @@ DATABASES = {
 
 # CORS Settings
 CORS_ALLOWED_ORIGINS = [
-    "http://better-play-erni.onrender.com",
+    "https://better-play-erni.onrender.com",
     "http://localhost:5173",  # Your React frontend development URL
     "http://127.0.0.1:5173",
     # Add your production frontend URL later
