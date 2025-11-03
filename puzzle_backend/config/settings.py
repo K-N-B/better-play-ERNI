fceb305bf85baffabb5c735cf68c0ec0d7153d91
Yughie <85423994+Yughie@users.noreply.github.com>
2025-11-03 11:37:49 +0800

    Setting configuration update

--- puzzle_backend/config/settings.py
+++ puzzle_backend/config/settings.py
@@ -25,7 +25,7 @@ load_dotenv(BASE_DIR / '.env')
 # SECURITY WARNING: keep the secret key used in production secret!
 # Provide a default for safety
 SECRET_KEY = os.environ.get("SECRET_KEY", "default-insecure-key-for-dev")
-DEBUG = False
+DEBUG = True
 
 
 ALLOWED_HOSTS = ["better-play-erni.onrender.com", "localhost", "127.0.0.1"]  # Add your production domain later
