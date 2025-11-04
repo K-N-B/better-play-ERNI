c37cb5c16d4211064b5f6ded83ea5995a6063d7e
Yughie <85423994+Yughie@users.noreply.github.com>
2025-11-04 12:00:23 +0800

    Configure urls

--- puzzle_backend/config/urls.py
+++ puzzle_backend/config/urls.py
@@ -37,7 +37,7 @@ urlpatterns = [
     path("api/gameplay/", include("gameplay.urls")),
 
     path('api/v1/cron/generate-puzzles/', cron_generate_puzzles_view, name='cron_task'),
-    path('healthz/', health_check, name='health-check') /
+    path('healthz/', health_check, name='health-check'), # Health check endpoint wakeup URL
 ]
 
 if settings.DEBUG:
