c93711f0776901c32ad4d90c339abc086e101569
gavin-cii <forondayna1214@gmail.com>
2025-11-12 13:54:53 +0800

    Removed extra daily ur

--- puzzle_backend/games/urls.py
+++ puzzle_backend/games/urls.py
@@ -1,14 +1,13 @@
 # games/urls.py
 from django.urls import path
 from .views import DailyPuzzlesView, GetSudokuHintLimitsView
-from . import views
 
 urlpatterns = [
     # Endpoint to get daily puzzles for a specific date (or today if none specified)
     # GET /api/games/daily/                  -> Today's puzzles
     # GET /api/games/daily/?date=2025-10-19 -> Puzzles for 2025-10-19
     path("daily/", DailyPuzzlesView.as_view(), name="daily-puzzles"),
-    path("daily/", views.DailyPuzzlesView.as_view(), name="daily-puzzles"),
+    # path("daily/", views.DailyPuzzlesView.as_view(), name="daily-puzzles"),
     # Endpoint to mock generate daily puzzles (FOR DEVELOPMENT ONLY)
     # POST /api/games/mock-generate/ with {"date": "YYYY-MM-DD"}
     # path(
