bd629f3c2a941770b4bc579d0f871d550dfbad7f
gavin-cii <forondayna1214@gmail.com>
2026-01-29 14:19:13 +0800

    Added view for email send cron

--- puzzle_backend/users/urls.py
+++ puzzle_backend/users/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path('api/departments/', views.DepartmentListView.as_view(), name='department-list'),
     path('me/complete-profile/', views.CompleteProfileView.as_view(), name='complete-profile'),
     path('users/me/', views.current_user_view, name='current-user'),
+    path('cron/reminders/', views.trigger_email_reminders, name='cron-reminders'),
 ]
